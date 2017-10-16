12d7043b2ded4c582c6f07ed7e14705e5a2f3da7
panakuma <kumachan05-11@hotmail.co.jp>
2017-10-16 18:54:28 +0900

    Update gcloud.sh

--- gcloud.sh
+++ gcloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export GOOGLE_APPLICATION_CREDENTIALS="./My First Project-084816f2fb65.json"
-export GCLOUD_PROJECT="eternal-tendril-178816"
+export GOOGLE_APPLICATION_CREDENTIALS="<Value>"
+export GCLOUD_PROJECT="<Value>"
 
 arecord -t raw -c 1 -r 16000 -f S16_LE -d 5 tweet.raw
 python speech-to-text.py tweet.raw
